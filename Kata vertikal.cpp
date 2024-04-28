557211fd8dd71825eeb21efb18394d4bf0f009d4
kartikaaadn <152579994+kartikaaadn@users.noreply.github.com>
2024-04-28 09:44:00 +0700

    Kata vertikal.cpp

--- Kata vertikal.cpp
+++ Kata vertikal.cpp
@@ -1,10 +1,1 @@
-bool Vertikal(const string& word, int startX, int startY) {
-    int wordLen = word.length();
-    for(int i = 0; i < wordLen; i++) {
-        int x = startX + i;
-        int y = startY;
-        if(x < 0 || x >= 24 || toupper(matrix[x][y]) != toupper(word[i]))
-            return false;
-    }
-    return true;
-}
+-
