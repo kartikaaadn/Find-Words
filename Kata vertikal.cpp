959c2be3b185a93a3415d9c423a085a6089031e2
Refkypirnado <162402539+Refkypirnado@users.noreply.github.com>
2024-04-28 13:35:58 +0700

    Kata vertikal.cpp

--- Kata vertikal.cpp
+++ Kata vertikal.cpp
@@ -1,1 +1,13 @@
-isi
+bool findVerticalWord(char matrix[NUM_ROWS][NUM_COLS], const string& word) {
+    for (int x = 0; x <= NUM_ROWS - word.length(); ++x) {
+        for (int y = 0; y < NUM_COLS; ++y) {
+            int z;
+            for (z = 0; z < word.length(); ++z) {
+                if (matrix[x + z][y] != word[z]) {
+                    break;
+                }
+            }
+            if (z == word.length()) return true;
+        }
+    }return false;
+}
