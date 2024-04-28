4259dfbae6ea435e68b79a5ca8a435417dc7f6e1
kartikaaadn <152579994+kartikaaadn@users.noreply.github.com>
2024-04-28 09:34:38 +0700

    kata_diag.cpp

--- kata_diag.cpp
+++ kata_diag.cpp
@@ -1,21 +1,55 @@
-bool DiagonalKananBawah(const string& word, int startX, int startY) {
-    int wordLen = word.length();
-    for(int i = 0; i < wordLen; i++) {
-        int x = startX + i;
-        int y = startY + i;
-        if(x < 0 || x >= 24 || y < 0 || y >= 24 || toupper(matrix[x][y]) != toupper(word[i]))
-            return false;
-    }
-    return true;
-}
+// mencari kata secara diagonal
 
-bool DiagonalKananAtas(const string& word, int startX, int startY) {
-    int wordLen = word.length();
-    for(int i = 0; i < wordLen; i++) {
-        int x = startX - i;
-        int y = startY + i;
-        if(x < 0 || x >= 24 || y < 0 || y >= 24 || toupper(matrix[x][y]) != toupper(word[i]))
-            return false;
+bool findDiagonalWord(char matrix[NUM_ROWS][NUM_COLS], const string& word) {
+    // Mencari diagonal kiri atas ke kanan bawah
+    for (int x = 0; x <= NUM_ROWS - word.length(); ++x) {
+        for (int y = 0; y <= NUM_COLS - word.length(); ++y) {
+            int z;
+            for (z = 0; z < word.length(); ++z) {
+                if (matrix[x + z][y + z] != word[z]) {
+                    break;
+                }
+            }
+            if (z == word.length()) return true;
+        }
     }
-    return true;
+
+    // Mencari diagonal kanan atas ke kiri bawah
+    for (int x = 0; x <= NUM_ROWS - word.length(); ++x) {
+        for (int y = NUM_COLS - 1; y >= word.length() - 1; --y) {
+            int z;
+            for (z = 0; z < word.length(); ++z) {
+                if (matrix[x + z][y - z] != word[z]) {
+                    break;
+                }
+            }
+            if (z == word.length()) return true;
+        }
+    }
+    
+    // Mencari diagonal kiri bawah ke kanan atas
+    for (int x = NUM_ROWS - 1; x >= word.length() - 1; --x) {
+        for (int y = 0; y <= NUM_COLS - word.length(); ++y) {
+            int z;
+            for (z = 0; z < word.length(); ++z) {
+                if (matrix[x - z][y + z] != word[z]) {
+                    break;
+                }
+            }
+            if (z == word.length()) return true;
+        }
+    }
+
+    // Mencari diagonal kanan bawah ke kiri atas
+    for (int x = NUM_ROWS - 1; x >= word.length() - 1; --x) {
+        for (int y = NUM_COLS - 1; y >= word.length() - 1; --y) {
+            int z;
+            for (z = 0; z < word.length(); ++z) {
+                if (matrix[x - z][y - z] != word[z]) {
+                    break;
+                }
+            }
+            if (z == word.length()) return true;
+        }
+    }return false;
 }
