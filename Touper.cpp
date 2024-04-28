3818b56ddc99c6a46c1d6e50f28c8404a24c9a0e
dwiandini01 <151293038+dwiandini01@users.noreply.github.com>
2024-04-28 09:58:15 +0700

    Touper.cpp

--- Touper.cpp
+++ Touper.cpp
@@ -1,3 +1,4 @@
+//Fungsi ini mengubah setiap huruf kecil dalam string menjadi huruf besar dan mengembalikan string yang telah dimodifikasi.
 string toUpperCase(string str) {
     for (char &c : str) {
         if (c >= 'a' && c <= 'z') {
